5d81ed1aaedd6341a36c81c510ea5d1f28f2ca9f
Jack Tanner <six2013@gmail.com>
2013-10-18 22:45:44 +0100

    Semi Working Player Animations

--- ItalianStickDudes/ItalianStickDudes/ItalianStickDudes/Sprites/Player.cs
+++ ItalianStickDudes/ItalianStickDudes/ItalianStickDudes/Sprites/Player.cs
@@ -14,6 +14,9 @@ namespace ItalianStickDudes
         private int PlayerNumber;
         private Vector2 Velocity;
 
+        private bool Running = false;
+        private bool Once = false;
+
         public Player()
         {
 
@@ -25,7 +28,7 @@ namespace ItalianStickDudes
             SpriteTexture = playerTexture;
             PlayerNumber = WhichPlayer;
             
-            InitializeAnimation(1, 2);
+            InitializeAnimation(1, 7);
             PlayAnimation(0, 2, 800);
         }
 
@@ -36,7 +39,42 @@ namespace ItalianStickDudes
 
          
             Velocity.X += gamePad.ThumbSticks.Left.X;
-            
+
+            if (gamePad.ThumbSticks.Left.X == 0.0f)
+            {
+                if (Velocity.X > 1.0f)
+                {
+                    Velocity.X -= 0.6f;
+                    Running = true;
+                }
+                else if (Velocity.X < -1.0f)
+                {
+                    Running = true;
+                    Velocity.X += 0.6f;
+                }
+
+                if (Velocity.X < 1.0f && Velocity.X > -1.0f)
+                {
+                    Velocity.X = 0.0f;
+                    Once = false;
+                    Running = false;
+                    PlayAnimation(0, 2, 800);
+                }
+            }
+
+            if (!Once)
+            {
+                if (Running)
+                {
+                    PlayAnimation(3, 6, 100);
+                    Once = true;
+                }
+            }
+
+            if (Velocity.X >= 30.0f)
+                Velocity.X = 30.0f;
+            else if (Velocity.X <= -30.0f)
+                Velocity.X = -30.0f;
 
             Position += Velocity;
         }
